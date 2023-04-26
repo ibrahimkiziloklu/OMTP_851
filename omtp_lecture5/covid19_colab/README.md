8650950003958187e0600326c2036c36bb397f11
mahed.dadgostar@gmail.com <mahed.dadgostar@gmail.com>
2023-04-26 11:53:05 +0200

    readme covid_colab added + omtp lecture 9 updated

---
+++ omtp_lecture5/covid19_colab/README.md
@@ -0,0 +1,2 @@
+# README
+You can find the output examples of this exercise in the ![examples](https://github.com/ibrahimkiziloklu/OMTP_851/tree/main/omtp_lecture5/covid19_colab/examples) folder.
--- omtp_lecture9/omtp_lectrue9.py
+++ omtp_lecture9/omtp_lectrue9.py
@@ -9,25 +9,17 @@ def create_env():
     return env
 
 
-def create_dummy_env(env):
-    """Create and return a dummy vectorized environment for compatibility with Stable Baselines 3."""
-    dummy_env = DummyVecEnv([lambda: env])
-    dummy_env.observation_space = env.observation_space
-    dummy_env.action_space = env.action_space
-    return dummy_env
-
-
-def train_dqn_model(env, total_timesteps=100000, log_dir='./logs'):
+def train_dqn_model(env, total_timesteps=300000, log_dir='./logs'):
     """Train a DQN model with the specified environment and return the trained model."""
     model = DQN("MlpPolicy", env, verbose=1, tensorboard_log=log_dir)
-    model.learn(total_timesteps=total_timesteps)
+    model.learn(total_timesteps=total_timesteps, tb_log_name='dqn_cartpole')
     return model
 
 
 def train_ppo_model(env, total_timesteps=100000, log_dir='./logs'):
     """Train a PPO model with the specified environment and return the trained model."""
     model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=log_dir)
-    model.learn(total_timesteps=total_timesteps)
+    model.learn(total_timesteps=total_timesteps, tb_log_name='ppo_cartpole')
     return model
 
 
@@ -53,30 +45,29 @@ def run_agent(model, env):
 
 
 def main():
-    algorithm = 'ppo'  # Choose between 'dqn' and 'ppo'
-    total_timesteps = 100000
+    algorithm = 'dqn'  # Choose between 'dqn' and 'ppo'
+    total_timesteps = 150000
     model_filename = f'{algorithm}_cartpole_{total_timesteps}'
     train_model_flag = True
     env = create_env()
-    dummy_env = create_dummy_env(env)
 
     if train_model_flag:
         if algorithm == 'dqn':
-            model = train_dqn_model(dummy_env,total_timesteps=total_timesteps)
+            model = train_dqn_model(env = env,total_timesteps=total_timesteps)
         elif algorithm == 'ppo':
-            model = train_ppo_model(dummy_env,total_timesteps=total_timesteps)
+            model = train_ppo_model(env = env,total_timesteps=total_timesteps)
         else:
             raise ValueError("Invalid algorithm. Choose either 'dqn' or 'ppo'.")
         model.save(model_filename)
     else:
         if algorithm == 'dqn':
-            model = load_dqn_model(model_filename, dummy_env)
+            model = load_dqn_model(model_filename, env)
         elif algorithm == 'ppo':
-            model = load_ppo_model(model_filename, dummy_env)
+            model = load_ppo_model(model_filename, env)
         else:
             raise ValueError("Invalid algorithm. Choose either 'dqn' or 'ppo'.")
 
-    run_agent(model, env)
+    #run_agent(model, env)
 
 
 if __name__ == '__main__':
